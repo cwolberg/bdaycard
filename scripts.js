3ac207c472fcb6e79ce716e035f75b7a9a162dcf
Chris Wolberg <cwolberg22@gmail.com>
2021-08-09 07:06:08 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.getElementById("canvas").style.display = "none";
+document.getElementById("happyBday").style.display = "none";
+document.getElementById("starwars").style.display = "none";
+
+function openCard() {
+    document.getElementById("happyBday").style.display = "block";
+    document.getElementById("preCard").style.display = "none";
+    document.getElementById("preCardBtn").style.display = "none";
+    setTimeout(function(){ document.getElementById("canvas").style.display = "initial"; }, 1400);
+    var audio = document.getElementById("audio");
+        audio.play();
+    setTimeout(function(){ document.getElementById("starwars").style.display = "initial"; }, 1400);
+  }
